8df11f4fdb006965f72fa0f839917e7e30735b9f
akngs <jania902@gmail.com>
2024-09-28 19:32:10 +0900

    Document error handling

--- JustPDF/JustPDFApp.swift
+++ JustPDF/JustPDFApp.swift
@@ -164,15 +164,25 @@ struct Document: FileDocument {
     var pdf: PDFDocument?
 
     init() { self.pdf = PDFDocument() }
+
     init(configuration: ReadConfiguration) throws {
-        if let data = configuration.file.regularFileContents { pdf = PDFDocument(data: data) }
+        guard let data = configuration.file.regularFileContents else { throw DocumentError.unableToReadFile }
+        guard let pdf = PDFDocument(data: data) else { throw DocumentError.invalidPDFData }
+        self.pdf = pdf
     }
 
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
-        guard let pdf = pdf else { throw CocoaError(.fileReadCorruptFile) }
-        guard let data = pdf.dataRepresentation() else { throw CocoaError(.fileReadCorruptFile) }
+        guard let pdf = pdf else { throw DocumentError.noPDFDocument }
+        guard let data = pdf.dataRepresentation() else { throw DocumentError.unableToCreateFileWrapper }
         return FileWrapper(regularFileWithContents: data)
     }
+}
+
+enum DocumentError: Error {
+    case unableToReadFile
+    case invalidPDFData
+    case noPDFDocument
+    case unableToCreateFileWrapper
 }
 
 #Preview {
