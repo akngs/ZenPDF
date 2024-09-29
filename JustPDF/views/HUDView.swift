3619f1a6a7db50296dcb4af566c13f46f025f3bd
akngs <jania902@gmail.com>
2024-09-29 10:34:17 +0900

    Hide the page number after 1 sec

--- JustPDF/views/HUDView.swift
+++ JustPDF/views/HUDView.swift
@@ -1,7 +1,8 @@
 import SwiftUI
-/// HUD showing the page number
+
 struct HUDView: View {
     var state: DocumentState
+    @State private var opacity: Double = 0
 
     var body: some View {
         VStack {
@@ -9,14 +10,24 @@ struct HUDView: View {
             HStack {
                 Spacer()
                 Text("\(state.pageNum)")
+                    .font(.system(size: 16, weight: .medium, design: .default))
+                    .monospacedDigit()
                     .padding(.vertical, 5)
                     .padding(.horizontal, 10)
                     .foregroundColor(.white)
                     .background(Color(.black).opacity(0.25),
                                 in: RoundedRectangle(cornerRadius: 10.0, style: .continuous))
                     .offset(x: -10, y: -10)
+                    .opacity(opacity)
             }
         }
+        .onChange(of: state.pageNum) { oldValue, newValue in updateHUD() }
+        .onAppear { updateHUD() }
+    }
+
+    private func updateHUD() {
+        withAnimation(.easeIn(duration: 0.3)) { opacity = 1 }
+        withAnimation(.easeOut(duration: 0.3).delay(1)) { opacity = 0}
     }
 }
 
